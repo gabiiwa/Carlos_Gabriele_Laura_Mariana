efe9a2f61cf2bc000a983ac5fcb568dfd0678341
gabiiwa <gabriele.cesar.iwashima@ice.ufjf.br>
2022-05-28 16:05:44 -0300

    registrando usuários e criando classe de postagens

--- django_api/sistema/admin.py
+++ django_api/sistema/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Usuario
+from .models import Estudante
+from .models import Professor
+from .models import Postagem
+from .models import PostagemProgramada
 
 # Register your models here.
+admin.site.register(Estudante)
+admin.site.register(Professor)
+admin.site.register(Postagem)
+admin.site.register(PostagemProgramada)
--- django_api/sistema/models.py
+++ django_api/sistema/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
     senha=models.CharField(max_length=20)
     cpf = models.IntegerField()
     nome = models.CharField(max_length=100)
@@ -14,3 +15,11 @@ class Professor(Usuario):
 class Estudante(Usuario):
     matricula = models.CharField(11)
 
+class Postagem(models.Model):
+    texto = models.CharField(max_length=10000)
+    fkusuario = models.ForeignKey(id_usuario, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.fkusuario.name
+class PostagemProgramada(Postagem):
+    post_date = models.DateField()
+    
