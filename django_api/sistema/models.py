3dfb07a32c404a0106e0f950d6cb080b1e29221d
gabiiwa <gabriele.cesar.iwashima@ice.ufjf.br>
2022-05-29 21:06:42 -0300

    corrigindo chave estrangeira de postagem e chave primária de usuário

--- django_api/sistema/models.py
+++ django_api/sistema/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Usuario(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    senha=models.CharField(max_length=20)
     cpf = models.IntegerField()
     nome = models.CharField(max_length=100)
+
+    class Meta:
+       abstract = True
+       ordering = ['nome'] 
+
+
     def __str__(self):
         return self.nome
 
@@ -13,13 +17,13 @@ class Professor(Usuario):
     siape = models.IntegerField()
 
 class Estudante(Usuario):
-    matricula = models.CharField(11)
+    matricula = models.CharField(max_length=11)
 
 class Postagem(models.Model):
     texto = models.CharField(max_length=10000)
-    fkusuario = models.ForeignKey(id_usuario, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.fkusuario.name
+    fkusuario = models.ForeignKey(Estudante,on_delete=models.CASCADE)
+    # def __str__(self):
+    #     return self.fkusuario.name
 class PostagemProgramada(Postagem):
     post_date = models.DateField()
     
