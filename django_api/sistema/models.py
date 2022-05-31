0e39d931ab18f522d0edf9b6e6d28fc168c5090e
gabiiwa <gabriele.cesar.iwashima@ice.ufjf.br>
2022-05-31 01:01:07 -0300

    inserindo atributos em professor, estudante e postagem programada

--- django_api/sistema/models.py
+++ django_api/sistema/models.py
@@ -18,11 +18,19 @@ class Professor(Usuario):
 
 class Estudante(Usuario):
     matricula = models.CharField(max_length=11)
+    pontuacao = models.IntegerField()
+    titulo = models.CharField(max_length=100)
 
 class Postagem(models.Model):
     texto = models.CharField(max_length=10000)
     fkusuario = models.ForeignKey(Estudante,on_delete=models.CASCADE)
     
-class PostagemProgramada(Postagem):
+class PostagemProgramada(models.Model):
+    texto = models.CharField(max_length=10000)
     post_date = models.DateField()
-    
+    fkusuario = models.ForeignKey(Professor,on_delete=models.CASCADE)
+    tag_choice = (
+        ("TG1", "Normal"),
+        ("TG2", "Extra"),
+    )
+    tag = models.CharField(choices=tag_choice, max_length=10, default="TG1") #marcador que pode ser de dois tipos: tarefa diária ou tarefa extra
