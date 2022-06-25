44f7b90d49934a65dced4adf05fc26880cdcb12f
gabiiwa <gabriele.cesar.iwashima@ice.ufjf.br>
2022-06-25 11:38:55 -0300

    home com django e views

--- django_api/sistema/views.py
+++ django_api/sistema/views.py
@@ -12,24 +12,37 @@ class PostagemViewSet(viewsets.ModelViewSet):
     def create(self, request):
         serializer = serializers.PostagemSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
+            #salva os dados no banco
              serializer.save()
+             #pega os dados que foram enviados pela requisição post
              data = request.data
+             #pega o id correspondente aestudante que fez o post para poder acessar a pontuação
              estudante_id = data.get("fkusuario")
              estudante_obj = models.Estudante.objects.get(id=estudante_id)
-             print("Pontos que quero inserir:{}".format(15))
-             print("\n Pontos que tenho {}".format(estudante_obj.pontuacao))
+             #atualizando a pontuação
              estudante_obj.pontuacao += 15
              estudante_obj.save()
              return Response(serializer.data)
 class PostagemArmazenadaViewSet(viewsets.ModelViewSet):
     queryset = models.PostagemArmazenada.objects.all().order_by('post_date')
     serializer_class = serializers.PostagemArmazenadaSerializer
+    # def create(self, request):
+    #     serializer = serializers.PostagemArmazenadaSerializer(data=request.data)
+    #     if serializer.is_valid(raise_exception=True):
+    #          serializer.save()
+             
+    #          return Response(serializer.data)
+    # def list(self, request):
+    #     #só postar se não for programada
+    #     #caso contrario, precisa comparar o dia e hora 
 
-
-####################Páginas########################
+#############Páginas que dependem de dados do banco###################
 def home(request):
     response = requests.get('http://127.0.0.1:8000/postagem/')
     data = response.json()
     estudantes = [models.Estudante.objects.get(id=dict_est['fkusuario']).nome for dict_est in data ]
-    print("\n lista de estudantes:{} \n \n".format(estudantes))
+    #inserindo qual foi a estudante que realizoua postagem
+    for post,i in zip(data,range(len(estudantes))):
+        post['nome']=estudantes[i]
+  
     return render(request, 'home.html', {'data': data})
